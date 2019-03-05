095692f51806c08e7fe9af4625d39c80861e3751
Zooheck <davidscottflack@gmail.com>
2019-03-05 15:54:46 -0600

    Build basic react app

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -2,11 +2,67 @@ import React, { Component } from 'react'
 import Display from './Display';
 
 export default class Dashboard extends Component {
+  state = {
+    balls: 0,
+    strikes: 0
+  }
+  strike = e => {
+    e.preventDefault()
+    if(this.state.strikes < 3) {
+      this.setState(prevState => {
+        return {
+          strikes: prevState.strikes + 1
+        }
+      }) 
+    } else if (this.state.strikes >= 3) {
+      this.setState({
+        strikes: 0,
+        balls: 0
+      })
+    }
+  }
+  ball = e => {
+    e.preventDefault()
+    if (this.state.balls < 4) {
+      this.setState(prevState => {
+        return {
+          balls: prevState.balls + 1
+        }
+      })
+    } else if (this.state.balls >= 4) {
+      this.setState({
+        strikes: 0,
+        balls: 0
+      })
+    }
+  }
+  foul = e => {
+    e.preventDefault()
+    if (this.state.strikes >= 2) {
+      return;
+    } else {
+      this.setState(prevState => {
+        return {
+          strikes: prevState.strikes + 1
+        }
+      })
+    }
+  }
+  hit = e => {
+    e.preventDefault()
+    this.setState({
+      balls: 0,
+      strikes: 0
+    })
+  }
   render() {
     return (
       <div>
-        <Display />
-        <h1>testing</h1>
+        <Display stats={this.state}/>
+        <button onClick={this.strike}>Strike</button>
+        <button onClick={this.ball}>Ball</button>
+        <button onClick={this.foul}>Foul</button>
+        <button onClick={this.hit}>Hit</button>
       </div>
     )
   }
--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Display = () => {
+const Display = (props) => {
+  const { stats } = props;
   return (
     <div>
-      
+      <h2>Balls: {stats.balls}</h2>
+      <h2>Strikes: {stats.strikes}</h2>
     </div>
   )
 }
