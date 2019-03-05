130f7f6d9ecc2bb5855d398eb8bee11c0c58dd7d
Zooheck <davidscottflack@gmail.com>
2019-03-05 15:32:29 -0600

    Fix yarn bug

---
+++ baseball/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Display from './Display';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <Display />
+        <h1>testing</h1>
+      </div>
+    )
+  }
+}
